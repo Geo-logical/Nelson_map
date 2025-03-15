01b604615b7b98f859d1f65ff869ee81c55f72bc
Geo-logical <mcgowan.andrew.eng@gmail.com>
2025-03-14 16:28:25 -1000

    updated attractions list

---
+++ data/Nelson_attractions_broken.js
@@ -0,0 +1,19 @@
+var geojsonData = {
+"type": "FeatureCollection",
+"name": "Nelson_attractions",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "name": "The Free House Pub", "site": "http://www.thefreehouse.co.nz/", "address": "95 Collingwood Street, Nelson 7010, New Zealand", "Type": "drink" }, "geometry": { "type": "Point", "coordinates": [ 173.287090957457366, -41.273296043240641 ] } },
+{ "type": "Feature", "properties": { "id": 2, "name": "The Indian Cafe", "site": "https://www.theindiancafe.co.nz/", "address": "94 Collingwood Street, Nelson 7010, New Zealand", "Type": "food" }, "geometry": { "type": "Point", "coordinates": [ 173.286734988155615, -41.273361976358217 ] } },
+{ "type": "Feature", "properties": { "id": 3, "name": "House", "site": null, "address": "Neale Ave.  Nelson NZ", "Type": "house" }, "geometry": { "type": "Point", "coordinates": [ 173.234997971200443, -41.308610171264085 ] } },
+{ "type": "Feature", "properties": { "id": 4, "name": "March Fest", "site": "https://www.marchfest.com/", "address": "Foundsers Park", "Type": "drink" }, "geometry": { "type": "Point", "coordinates": [ 173.295528779556292, -41.261700922676035 ] } },
+{ "type": "Feature", "properties": { "id": 5, "name": "McCashins Brewerty", "site": "https://mccashins.co.nz/", "address": "660 Main Road Stoke, Stoke, Nelson 7001, New Zealand", "Type": "drink" }, "geometry": { "type": "Point", "coordinates": [ 173.22570354524899, -41.318645298511832 ] } },
+{ "type": "Feature", "properties": { "id": 6, "name": "Gravity", "site": "http://www.gravitynelson.co.nz/", "address": "140 Bridge Street, Nelson 7010, New Zealand", "Type": "gear" }, "geometry": { "type": "Point", "coordinates": [ 173.28639778737778, -41.272722612501319 ] } },
+{ "type": "Feature", "properties": { "id": 7, "name": "Kiwi Journeys", "site": "http://www.kiwijourneys.co.nz/", "address": "16B Paru Paru Road, Nelson 7010, New Zealand", "Type": "gear" }, "geometry": { "type": "Point", "coordinates": [ 173.281723159704512, -41.269212022472388 ] } },
+{ "type": "Feature", "properties": { "id": 8, "name": "Velodrome", "site": null, "address": null, "Type": "gear" }, "geometry": { "type": "Point", "coordinates": [ 173.206849403427725, -41.332708666228129 ] } },
+{ "type": "Feature", "properties": { "id": 9, "name": "Woolsworth", "site": null, "address": "12 Putaitai Street, Stoke, Nelson 7011, New Zealand", "Type": "food" }, "geometry": { "type": "Point", "coordinates": [ 173.234142997702094, -41.311732777898143 ] } },
+{ "type": "Feature", "properties": { "id": 10, "name": "Hopgoods Restaurant", "site": "https://www.hopgoods.co.nz/", "address": "284 Trafalgar Street, Nelson 7010, New Zealand", "Type": "food" }, "geometry": { "type": "Point", "coordinates": [ 173.28366488560664, -41.274787013851437 ] } },
+{ "type": "Feature", "properties": { "id": 11, "name": "Hawkers House", "site": "https://www.hawkerhouse.co.nz/", "address": "296 Trafalgar Street, Nelson 7010, New Zealand", "Type": "food" }, "geometry": { "type": "Point", "coordinates": [ 173.283635126843791, -41.275000726673653 ] } },
+{ "type": "Feature", "properties": { "id": 12, "name": "Melrose House", "site": "http://melrosecafe.co.nz/", "address": "26 Brougham Street, Nelson South, Nelson 7010, New Zealand", "Type": "food" }, "geometry": { "type": "Point", "coordinates": [ 173.28427659350956, -41.282194488456042 ] } }
+]
+};
